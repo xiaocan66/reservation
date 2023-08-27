78acdf05d9fc9945d44e57d3d3e7bb6702b22a80
lizican <lizican123@gmail.com>
2023-08-27 22:31:32 +0800

    update abi build

--- abi/build.rs
+++ abi/build.rs
@@ -1,7 +1,11 @@
+use std::process::Command;
+
 fn main() {
     tonic_build::configure()
         .out_dir("src/pb")
         .compile(&["protos/reservation.proto"], &["protos"])
         .unwrap();
+    //执行cargo fmt
+    Command::new("cargo").args(["fmt"]).output().unwrap();
     println!("cargo:rerun-if-changed=protos/reservation.proto");
 }
